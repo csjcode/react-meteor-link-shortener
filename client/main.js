905c5767087b1c8d855c16f339683328fee1d65a
csjcode <christophersstjohn@gmail.com>
2016-06-05 12:12:44 +0700

    LinkCreate component setup/render

--- client/main.js
+++ client/main.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header.js';
+import LinkCreate from './components/link_create.js';
 
 const App = () => {
    return (
       <div>
          <Header />
+         <LinkCreate />
       </div>
 
    );
