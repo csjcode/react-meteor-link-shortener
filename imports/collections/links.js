63afdb4fdb5a80413d977124cb428315cfec2ab3
csjcode <christophersstjohn@gmail.com>
2016-06-06 22:50:16 +0700

    Added validUrl module and Meteor check, Match

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,8 +1,11 @@
 import { Mongo } from 'meteor/mongo';
+import validUrl from 'valid-url';
+import { check, Match } from 'meteor/check'
 
 Meteor.methods({
    'links.insert': function (url) {
-      console.log('attempting to save ', url)
+      validUrl.isUri(url);
+      check(url,Match.Where(url => validUrl.isUri(url)));
    }
 });
 
